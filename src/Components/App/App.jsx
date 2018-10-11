b1e9a5e22a2d561668c9d2c8f778b7e79283962a
aditiverma1009 <aditiverma1009@gmail.com>
2018-10-11 11:36:50 +0530

    provider

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,42 +3,30 @@ import './App.css';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
-import {onClickEditReducer} from '../../redux/Actions/index';
+// import { onClickEditReducer } from '../../redux/Actions/index';
 
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
-    
-
-        
-    render() {
-      return (
-        <div className="shopping-list">
-          <h1>Shopping List yaynpm</h1>
-          <ul>
-            <li>Instagram</li>
-            <li>WhatsApp</li>
-            <li>Oculus</li>
-          </ul>
-        </div>
-      );
-    } 
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
   }
 
 
-const mapStateToProps = state => ({
+  render() {
+    return (
+      <div className="app" />
+    );
+  }
+}
 
-  });
-  
-  const mapDispatcherToProps = dispatch => ({
-   
-  });
-  export default withRouter(connect(mapStateToProps, mapDispatcherToProps)(App));
-  
-  
-  
 
+const mapStateToProps = () => ({
+
+});
+
+const mapDispatcherToProps = () => ({
+
+});
+export default withRouter(connect(mapStateToProps, mapDispatcherToProps)(App));
