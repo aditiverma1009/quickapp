cdb616c74e948ec05d451606dafcd5b00085ff4e
Nidhi Jagadeesha <nidhijune14@gmail.com>
2018-10-11 14:44:49 +0530

    modal

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,44 +1,58 @@
 import React from 'react';
+import Modal from "react-responsive-modal";
+import template from "../../download.jpeg";
 import './App.css';
-import axios from 'axios';
-import { connect } from 'react-redux';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import {onClickEditReducer} from '../../redux/Actions/index';
 
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  };
 
-        
-    render() {
-      return (
-        <div className="shopping-list">
-          <h1>Shopping List yaynpm</h1>
-          <ul>
-            <li>Instagram</li>
-            <li>WhatsApp</li>
-            <li>Oculus</li>
-          </ul>
-        </div>
-      );
-    } 
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+  render() {
+    const { open } = this.state;
+    return (
+      <div>
+        <button onClick={this.onOpenModal}>
+          Click me!
+      </button>
+        <Modal open={open} onClose={this.onCloseModal} center>
+          <div className="template-heading">
+            <center><h2>Templates</h2></center>
+          </div>
+          <label >Name</label>
+          <input type="text" width="100px" className="name-input" />
+          <div className="tile-container">
+            <div className="tile" onClick={() => alert("you have chose template 1")}>
+              <img src={template} width="100%" height="100%" />
+            </div>
+            <div className="tile-disabled">
+              AVAILABLE ON SPONSORSHIP
+            </div>
+          </div>
+          <div className="tile-container">
+            <div className="tile-disabled">
+              AVAILABLE ON SPONSORSHIP
+            </div>
+            <div className="tile-disabled">
+              AVAILABLE ON SPONSORSHIP
+            </div>
+          </div>
+          <center><button className="choose-template-button">Submit</button></center>
+        </Modal>
+      </div>
+    );
   }
+}
 
 
-const mapStateToProps = state => ({
-
-  });
-  
-  const mapDispatcherToProps = dispatch => ({
-   
-  });
-  export default withRouter(connect(mapStateToProps, mapDispatcherToProps)(App));
-  
-  
-  
-
+export default App;
