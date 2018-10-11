e8122956fd5ba5e3c9bc9b5096cda365df85e97e
Vishal Vasnani <vishalvasnani123@gmail.com>
2018-10-12 03:17:55 +0530

    Add template 1 with edit functionality

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import Canvas from '../Canvas/Canvas';
-import EditOptions from '../EditOptions/EditOptions';
 import './Body.css';
 
-const Body = () => (
-  <div className="BodyArea">
-    <Canvas />
-    <EditOptions />
-  </div>
-);
+export default class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-Body.propTypes = {
-};
-
-export default Body;
+  render() {
+    const {
+      fontSizeHead, fontSizeList0, fontSizeList1, fontSizeList2, fontSizeList3,
+    } = this.state;
+    return (
+      <div className="BodyArea">
+        <Canvas
+          fontSizeHead={Number(fontSizeHead)}
+          fontSizeList0={Number(fontSizeList0)}
+          fontSizeList1={Number(fontSizeList1)}
+          fontSizeList2={Number(fontSizeList2)}
+          fontSizeList3={Number(fontSizeList3)}
+        />
+      </div>
+    );
+  }
+}
