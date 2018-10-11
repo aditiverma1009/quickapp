75cd8c7a1184c7dc54e2f7caf18e12d2acc86224
aditiverma1009 <aditiverma1009@gmail.com>
2018-10-11 10:27:06 +0530

    Redux used

---
+++ src/Render.js
@@ -0,0 +1,20 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import store from './redux/store';
+import App from '../src/Components/App/App';
+
+const Render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'),
+  );
+};
+
+
+export default Render;
---
+++ src/redux/Reducers/noteReducer.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const defaultValue = {
+ 
+};
+
+const noteReducer = (prevState = defaultValue, action) => {
+  switch (action.type) {    
+
+  
+
+    default: {
+  
+    }
+
+  }
+};
+export default noteReducer;
+
