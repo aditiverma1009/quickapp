8edce4a3929942f5fef874d0e7e62f213fb1e01e
aditiverma1009 <aditiverma1009@gmail.com>
2018-10-12 10:36:21 +0530

    add project modal with verification

--- src/Components/new-ppt-card/new-ppt-card.jsx
+++ src/Components/new-ppt-card/new-ppt-card.jsx
@@ -1,15 +1,82 @@
 import React, { Component } from 'react';
 import './new-ppt-card.css';
+import Modal from 'react-responsive-modal';
 import logo from './logo.png';
+import { connect } from 'react-redux';
+import { Switch, Route, withRouter } from 'react-router-dom';
+
+import { addProject } from '../../redux/Actions';
 
 class NewPPTCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      acceptedPassword: true,
+      projectID: '',
+    };
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+  onProjectNameChange=(event)=>{
+    let ProjectNameValue = event.target.value;
+    let {projectList}= this.props;
+    if(projectList.length!=0){
+      let found = projectList.find((element)=>element.projectID === ProjectNameValue); 
+      if(found){
+        this.setState({
+          acceptedPassword:false,
+          projectID:ProjectNameValue,
+        })
+      }
+      else{
+        this.setState({
+          acceptedPassword:true,
+          projectID:ProjectNameValue,
+        })
+      }
+    }
+  }
+  onProjectSubmit=()=>{
+    let {projectID}=this.state;
+    let {acceptedPassword}=this.state;
+
+  }
+
   render() {
+
+    const { open } = this.state;
+    const { projectID } = this.state;
+    const { acceptedPassword } = this.state;
+    // console.log('pro',projectID)
     return (
       <div className="presentation-card">
-      <img className="new-ppt-card-icon" src={logo} alt="logo"/>
+        <img className="new-ppt-card-icon" src={logo} alt="logo" onClick={this.onOpenModal} />
+        <Modal open={open} onClose={this.onCloseModal} center>
+          <div className="template-heading">
+            <h2>Create Project</h2>
+            <label>Project Name:</label>
+            <input type="text" name="project-name" onChange={(event)=>this.onProjectNameChange(event)} /> 
+            <div className={this.state.projectID.length!=0?'VisibleStatus':'InvisibleStatus'}>{this.state.acceptedPassword  ? '' : 'This Project Name is taken'}</div>
+            <br/>
+            <button type="submit" disabled={!this.state.acceptedPassword || this.state.projectID.length==0} onClick={()=>this.props.addProject(projectID)}>Submit</button>
+          </div>
+        </Modal>
       </div>
     );
   }
 }
 
-export default NewPPTCard;
+const mapStateToProps = (state) => ({
+  projectList: state.templatesReducer.allProjects,
+});
+
+const mapDispatcherToProps = (dispatch) => ({
+  addProject: newProject => dispatch(addProject(newProject)),
+});
+export default withRouter(connect(mapStateToProps, mapDispatcherToProps)(NewPPTCard));
